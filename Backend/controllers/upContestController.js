5b4e58474ef1d77a642956dd3253ef3dc1fbd0d3
Shuvo Malakar <short_bit@Shuvos-MacBook-Air-2.local>
2025-02-13 08:57:56 +0600

    Build custom puppeteer

--- Backend/controllers/upContestController.js
+++ Backend/controllers/upContestController.js
@@ -147,7 +147,7 @@ async function fetchUpcomingCodechefContests() {
         });*/
 
         browser = await puppeteer.launch({ 
-            executablePath:'/opt/render/.cache/puppeteer/chrome/linux-133.0.6943.53/chrome-linux64/chrome',
+            ///executablePath:'/opt/render/.cache/puppeteer/chrome/linux-133.0.6943.53/chrome-linux64/chrome',
             args: ['--no-sandbox', '--disable-setuid-sandbox'],
             headless: true 
         });
